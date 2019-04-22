56b2dc9832236a4702550f4f8b503e6732508bf4
andy <nguyen>
2019-04-21 21:21:13 -0700

    Properly implement bubble sort

--- src/BubbleSorter.java
+++ src/BubbleSorter.java
@@ -4,10 +4,10 @@ public class BubbleSorter {
      * Sorts via bubble sort the given MyArray
      */
     public void sort(MyArray array, WindowManager wm) {
-        for (int i = 0; i < array.get_array_length(); i++) {
-            for (int j = i + 1; j < array.get_array_length(); j++) {
-                if (array.get_value(j) < array.get_value(i)) {
-                    array.swap(i, j);
+        for (int i = array.get_array_length() - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array.get_value(j) > array.get_value(j + 1)) {
+                    array.swap(j + 1, j);
                     wm.repaint();
                 }
             }
